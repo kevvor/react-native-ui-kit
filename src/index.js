92fc150e7044a05259283c496bb26c3abdbb3458
Keano <keanov@gmail.com>
2018-03-20 16:41:33 -0700

    Init repo/ Hello, World!

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const helloWorld = () => (
+    <View>
+        <Text>Hello, World!</Text>
+    </View>
+);
+
+export default helloWorld;
