fcc5b3db383e53f634b9e9df1f6a19cbf4031b5f
Keano <keanov@gmail.com>
2018-03-21 12:39:00 -0700

    Can Hello World!

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const helloWorld = () => (
-    <View>
-        <Text>Hello, World!</Text>
-    </View>
-);
+const helloWorld = () => {
+    return (
+        <View>
+            <Text>Hello, World!</Text>
+        </View>
+    )
+};
 
 export default helloWorld;
